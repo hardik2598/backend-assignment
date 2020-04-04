b385736450a1d078574ec6c42d3062288cac62c3
hardik2598 <hardikbhatt2598@gmail.com>
2020-04-04 23:59:25 +0530

    Active user from intercontroller service

--- src/main/java/com/postman/slotbookingsystem/controller/SlotBookingController.java
+++ src/main/java/com/postman/slotbookingsystem/controller/SlotBookingController.java
@@ -1,7 +1,7 @@
 package com.postman.slotbookingsystem.controller;
 
 import com.postman.slotbookingsystem.model.RequestSlot;
-import com.postman.slotbookingsystem.model.User;
+import com.postman.slotbookingsystem.service.InterControllerMap;
 import com.postman.slotbookingsystem.service.SlotManagerImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,22 +13,20 @@ import java.util.Map;
 @RestController
 @RequestMapping("/logged-in")
 public class SlotBookingController {
-
-    private User activeUser;
-
     private Map<String, String> url2UrlMapping;
 
     private Map<String, Object> url2ObjectMapping;
 
     @Bean
     public void mapInit() {
-        // to do
+        url2UrlMapping = InterControllerMap.getUrl2UrlMapping();
+        url2ObjectMapping = InterControllerMap.getUrl2ObjectMapping();
     }
 
     @RequestMapping("/book-slot")
     public String bookSlot() {
         RequestSlot requestSlot = (RequestSlot) url2ObjectMapping.get(url2UrlMapping.get("/logged-in/book-slot"));
-        SlotManagerImpl slotManagerInstance = SlotManagerImpl.getSlotManagerInstance(activeUser);
+        SlotManagerImpl slotManagerInstance = SlotManagerImpl.getSlotManagerInstance(InterControllerMap.getActiveUser());
         if (slotManagerInstance.bookSlot(requestSlot)) {
             return "Successfully book slot on date: " + requestSlot.getSlotDate()
                     + " with startTime: " + requestSlot.getSlotStartTime()
@@ -42,7 +40,7 @@ public class SlotBookingController {
     @RequestMapping("/check-slot-availability")
     public String checkSlotAvailability() {
         RequestSlot requestSlot = (RequestSlot) url2ObjectMapping.get(url2UrlMapping.get("/logged-in/check-slot-availability"));
-        SlotManagerImpl slotManagerInstance = SlotManagerImpl.getSlotManagerInstance(activeUser);
+        SlotManagerImpl slotManagerInstance = SlotManagerImpl.getSlotManagerInstance(InterControllerMap.getActiveUser());
         if (slotManagerInstance.checkAvailabilityOfSlot(requestSlot)) {
             return "Slot available for date: " + requestSlot.getSlotDate()
                     + " with startTime: " + requestSlot.getSlotStartTime()
@@ -56,7 +54,7 @@ public class SlotBookingController {
     @RequestMapping("/mark-slot-available")
     public String markSlotAvailable() {
         RequestSlot requestSlot = (RequestSlot) url2ObjectMapping.get(url2UrlMapping.get("/logged-in/mark-slot-available"));
-        SlotManagerImpl slotManagerInstance = SlotManagerImpl.getSlotManagerInstance(activeUser);
+        SlotManagerImpl slotManagerInstance = SlotManagerImpl.getSlotManagerInstance(InterControllerMap.getActiveUser());
         if (slotManagerInstance.markSlotAvailability(requestSlot)) {
             return "Slot marked available for date: " + requestSlot.getSlotDate()
                     + " with startTime: " + requestSlot.getSlotStartTime()
@@ -70,7 +68,7 @@ public class SlotBookingController {
     @RequestMapping("/mark-all-slot-available")
     public String markAllSlotAvailable() {
         RequestSlot requestSlot = (RequestSlot) url2ObjectMapping.get(url2UrlMapping.get("/logged-in/mark-all-slot-available"));
-        SlotManagerImpl slotManagerInstance = SlotManagerImpl.getSlotManagerInstance(activeUser);
+        SlotManagerImpl slotManagerInstance = SlotManagerImpl.getSlotManagerInstance(InterControllerMap.getActiveUser());
         slotManagerInstance.markAllSlotsAvailable(requestSlot.getSlotDate());
         return "All slots marked as available for date: " + requestSlot.getSlotDate();
     }
@@ -78,7 +76,7 @@ public class SlotBookingController {
     @RequestMapping("/get-all-available-slots")
     public List<RequestSlot> getAllAvailableSlots() {
         RequestSlot requestSlot = (RequestSlot) url2ObjectMapping.get(url2UrlMapping.get("/logged-in/get-all-available-slots"));
-        SlotManagerImpl slotManagerInstance = SlotManagerImpl.getSlotManagerInstance(activeUser);
+        SlotManagerImpl slotManagerInstance = SlotManagerImpl.getSlotManagerInstance(InterControllerMap.getActiveUser());
         return slotManagerInstance.getAllAvailableSlotForDate(requestSlot.getSlotDate());
     }
 }
